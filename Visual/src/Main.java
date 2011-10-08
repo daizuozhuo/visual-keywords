3926b934049012cc029c55e75ff3c9b031fefab8
Dai Zuozhuo <daizuozhuo@gmail.com>
2011-10-08 22:50:37 +0800

    can change bacground,mach faster algorithm.

--- Visual/src/Main.java
+++ Visual/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 
+
 public class Main {
     
 	public static void main(String[] args) {
--- Visual/src/Painter.java
+++ Visual/src/Painter.java
@@ -22,9 +22,8 @@ public class Painter {
 	private BufferedImage img = new BufferedImage(1600, 900, BufferedImage.TYPE_INT_ARGB);
 	Graphics g = img.createGraphics();
 	private final static FontRenderContext context = new FontRenderContext (null, false, false);
-	private static Point p_max=new Point(width/2,height/2);
-	private static Point p_min=new Point(width/2,height/2);
 	private static Point p_cen=new Point(width/2,height/2);
+	private static int min_size=0;
 	
 	public Painter(Word[] result) 
 	{
@@ -47,13 +46,14 @@ public class Painter {
 
 		reset_count(); // Reset make is the size of the font
 		
-		for (int i = 0; i < 50 && i < words.length; i++)
+		for (int i = 0; i < 150 && i < words.length; i++)
 		{
 			paint_str(words[i]); // paint the keywords one by one
 			System.out.println((i + 1 ) + " / " + words.length + " done.");
 			window.update();
 		}		
-		
+		set_bacground();
+		window.update();
 		// Save the picture
 		File outputfile = new File("res/output.gif");  
         ImageIO.write(img, "gif", outputfile);	
@@ -77,8 +77,24 @@ public class Painter {
 		}
 		
 	}
-
-	private void paint_str(Word word)
+	
+	//set bacground image;
+	private void set_bacground(){
+		BufferedImage bimg = null;
+		try {
+			bimg = ImageIO.read(new File("res/background.jpg"));
+		} catch (IOException e) {
+			System.out.println("use white bacground");
+		}
+		for(int i=0;i<width;i++) {
+			for(int j=0;j<height;j++) {
+				if(img.getRGB(i,j)==Color.white.getRGB()) {
+					img.setRGB(i, j, bimg.getRGB(i,j));
+				}
+			}
+		}
+	}
+	private boolean paint_str(Word word)
 	{
 		// Set the font
 		Font font = new Font(fontfile, Font.BOLD, word.get_count());
@@ -92,10 +108,10 @@ public class Painter {
 		
 		// Try to find an empty space of the string
 		Point position = search_space(bounds);
-		
+		if(position.x>width)return false;
 		// Draw the string
 		g.drawString(word.get_str(), (int) (position.x + 5 - bounds.getMinX()), (int) (position.y + 5 - bounds.getMinY()));
-		
+		return true;
 //		// The bounds of the string
 //		g.drawRect(position.x, position.y, 
 //				(int) (10 + bounds.getMaxX() - bounds.getMinX()),
@@ -108,23 +124,43 @@ public class Painter {
 		int str_X = (int) (bounds.getMaxX() - bounds.getMinX()) + 10;
 		int str_Y = (int) (bounds.getMaxY() - bounds.getMinY()) + 10;
 
-		// A random position to start searching
-		//int x = (int) (Math.random() * (width - str_X));
-		//int y = (int) (Math.random() * (height - str_Y));
 		int loop=str_Y;
-		int y=p_cen.y-loop-1;	
-		int x=p_cen.x-loop-1;
+		int step=(int)(0.1*str_Y);
+		if(step<1)step=1;
+		int y=p_cen.y-loop;	
+		int x=p_cen.x-loop;
 		// The starting position of x and y
 		int init_Y = y;
 		int init_X = x;
+		int left_bound=0;
+		int right_bound=0;
+		int up_bound=0;
+		int low_bound=0;
 		
 		do
 		{
 			boolean found = true;
+			System.out.println(str_X+" "+str_Y+" "+min_size+" "+step);
+			
+			if(min_size!=0){
+				if(str_X>=min_size&&str_Y>=min_size)
+				{
+					found=false;
+					break;
+				}
+			}
 			for (int i = 0; i < str_Y; i++)
 			{
 				for (int j = 0; j < str_X; j++)
 				{
+					if (x + j >= img.getWidth()) {
+						found = false;
+						break;
+					}
+					if (y + i >= img.getHeight()) {
+						found = false;
+						break;
+					}
 					if (img.getRGB(x + j, y + i) != Color.white.getRGB()) 
 					{
 						found = false;
@@ -133,44 +169,58 @@ public class Painter {
 					}
 				}
 			}
-			if (found)
+			if (found) return new Point(x, y);
+			left_bound=p_cen.x-loop;
+			right_bound=p_cen.x+loop;
+			low_bound=p_cen.y-loop;
+			up_bound=p_cen.y+loop;
+			if(low_bound<=1)low_bound=step;
+			if(up_bound>=height-str_Y)up_bound=height-str_Y;
+			//System.out.println("x "+x+" y "+y+" l "+left_bound+" r "+right_bound+" l "+low_bound+" u "+up_bound);
+			if(x<=left_bound)
 			{
-				
-				return new Point(x, y);
-			}
-			//System.out.println(x+" "+y+" "+loop);
-			if(x<p_cen.x-loop)
-			{
-				if(y>=p_cen.y-loop)y--;
-				else x++;
-			}else if(x>p_cen.x+loop)
+				if(y>low_bound)y=y-step;
+				else x=x+step;
+			}else if(x>=right_bound)
 			{ 
-				if(y<=p_cen.y+loop)y++;
-				else x--;
+				if(y<up_bound)y=y+step;
+				else x=x-step;
 			}else
 			{
-				if(y<=p_cen.y+loop)
+				if(y<=low_bound)
 				{
-					x++;
-				}else if(y>=p_cen.y-loop)
+					x=x+step;
+				}else if(y>=up_bound)
 				{
-					x--;
+					x=x-step;
+				}else{
+					x=x+step;
 				}
 			}
-			
+			System.out.println(x+" "+y+" "+init_X+" "+init_Y+" "+loop);
 			//System.out.println(x+" "+y);
 			//System.out.println();
-			if(x==init_X&&y==init_Y)
+			if(x<=init_X&&y<=init_Y)
 			{
-				init_X--;
-				init_Y--;
-				loop++;
-				//System.out.println("loop "+loop);
+				init_X=left_bound-step;
+				init_Y=low_bound-step;
+				if(init_Y<=1)init_Y=step;
+				loop=loop+step;
+				System.out.println("loop "+loop);
 			}
-			if(x>width-str_X||y>height-str_Y)break;
+			
+			if(x>width-str_X)break;
 		}	while (loop<500);	
 		
 		System.out.println("Error! No space available!");
+		if(str_X<str_Y){
+			if(min_size==0)min_size=str_X;
+			if(min_size>str_X)min_size=str_X;
+		}else{
+			if(min_size==0)min_size=str_Y;
+			if(min_size>str_Y)min_size=str_Y;
+		}
+		
 		
 		// Some where outside the picture
 		return new Point(width + 100, height + 100);
