93a7f6a742f117ae9feedf04f4518e50b856e969
liligeng111 <liligeng111@gmail.com>
2011-11-09 18:45:59 +0800

    new catcher for multi file

--- Visual/src/Word.java
+++ Visual/src/Word.java
@@ -4,15 +4,17 @@ import java.awt.Polygon;
 
 public class Word implements Comparable<Word> {
     private int count; // Times this key words was found
+    private int count2; // Times this key words was found in file 2
     private String str; // The key word
     private int size; // Font size
     private int x; // x coordinate
     private int y; // y coordinate
     private Polygon bounds;
    
-    public Word(String str)
+    public Word(String str, int i)
     {
-        this.count = 1;
+        if (i == 1) this.count = 1;
+        else count2++;
         this.str = str;
         this.size = 15;
         this.x = -1;
@@ -21,8 +23,10 @@ public class Word implements Comparable<Word> {
 
     public String getStr() {return str;}
     public int getCount() {return count;}
+    public int getCount2() {return count2;}
     public int getSize() {return size;}
     public void hit() {count++;} // Found again
+    public void hit2() {count2++;} // Found again in file 2
     public void setSize(int i) {size = i;}
     public void setPoint(int x, int y) {this.x = x;    this.y = y;}
     public void setBounds(Polygon bounds) {this.bounds = bounds;}
